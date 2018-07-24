8ceb41f37ec987b8adee31b50dae38c2bbbdf89a
Liu-Cheng Xu <xuliuchengxlc@gmail.com>
2018-07-24 10:52:22 +0800

    Refactor hammerspoon

--- hammerspoon/Spoons/SpeedMenu.spoon/init.lua
+++ hammerspoon/Spoons/SpeedMenu.spoon/init.lua
@@ -2,7 +2,7 @@
 ---
 --- Menubar netspeed meter
 ---
---- based on official Hammerspoon/Spoons/SpeedMenu.spoon
+--- Based on official Hammerspoon/Spoons/SpeedMenu.spoon
 ---
 --- Download: [https://github.com/Hammerspoon/Spoons/raw/master/Spoons/SpeedMenu.spoon.zip](https://github.com/Hammerspoon/Spoons/raw/master/Spoons/SpeedMenu.spoon.zip)
 
@@ -18,9 +18,10 @@ obj.license = "MIT - https://opensource.org/licenses/MIT"
 
 function obj:init()
     self.menubar = hs.menubar.new()
-    obj:rescan()
+    obj:reload()
 end
 
+--- delta bytes
 local function format(delta)
     local str = ''
     if delta < 1024 then
@@ -33,43 +34,40 @@ local function format(delta)
     return str
 end
 
-local function data_diff()
-    local in_seq = hs.execute(obj.incmd)
-    local out_seq = hs.execute(obj.outcmd)
-    local in_delta = in_seq - obj.inseq
-    local out_delta = out_seq - obj.outseq
-    local instr = format(in_delta)
-    local outstr = format(out_delta)
-    obj.kbout = outstr
-    obj.kbin = instr
-    local title = '⇧ ' .. obj.kbout .. '\n⇩ ' .. obj.kbin
+-- Update network stats
+local function update()
+    local ibytes = hs.execute(obj.incmd)
+    local obytes = hs.execute(obj.outcmd)
+    local in_per_sec = format(ibytes - obj.old_ibytes)
+    local out_per_sec = format(obytes - obj.old_obytes)
+
+    local text = '⇧ ' .. out_per_sec .. '\n⇩ ' .. in_per_sec
     local hex_color = "#000000"
-    if obj.darkmode then
+
+    local darkmode = hs.osascript.applescript('tell application "System Events"\nreturn dark mode of appearance preferences\nend tell')
+    if darkmode then
         hex_color = "#FFFFFF"
     end
-    obj.title = hs.styledtext.new(title, {font={size=9.0, color={hex=hex_color}}})
-    obj.menubar:setTitle(obj.title)
-    obj.inseq = in_seq
-    obj.outseq = out_seq
+    local title = hs.styledtext.new(text, {font={size=9.0, color={hex=hex_color}}})
+
+    obj.menubar:setTitle(title)
+
+
+    obj.old_ibytes = ibytes
+    obj.old_obytes = obytes
 end
 
---- SpeedMenu:rescan()
---- Method
---- Redetect the active interface, darkmode …And redraw everything.
----
-
-function obj:rescan()
-    obj.interface = hs.network.primaryInterfaces()
-    obj.darkmode = hs.osascript.applescript('tell application "System Events"\nreturn dark mode of appearance preferences\nend tell')
+local function menu()
+    local primary_interface = hs.network.primaryInterfaces()
 
     local menuitems_table = {}
-    if obj.interface then
+    if primary_interface then
         -- Inspect active interface and create menuitems
-        local interface_detail = hs.network.interfaceDetails(obj.interface)
+        local interface_detail = hs.network.interfaceDetails(primary_interface)
         if interface_detail.AirPort then
             local ssid = interface_detail.AirPort.SSID
             table.insert(menuitems_table, {
-                title = "SSID: " .. ssid,
+                title = "SSID:     " .. ssid,
                 tooltip = "Copy SSID to clipboard",
                 fn = function() hs.pasteboard.setContents(ssid) end
             })
@@ -77,7 +75,7 @@ function obj:rescan()
         if interface_detail.IPv4 then
             local ipv4 = interface_detail.IPv4.Addresses[1]
             table.insert(menuitems_table, {
-                title = "IPv4: " .. ipv4,
+                title = "IPv4:     " .. ipv4,
                 tooltip = "Copy IPv4 to clipboard",
                 fn = function() hs.pasteboard.setContents(ipv4) end
             })
@@ -85,36 +83,46 @@ function obj:rescan()
         if interface_detail.IPv6 then
             local ipv6 = interface_detail.IPv6.Addresses[1]
             table.insert(menuitems_table, {
-                title = "IPv6: " .. ipv6,
+                title = "IPv6:     " .. ipv6,
                 tooltip = "Copy IPv6 to clipboard",
                 fn = function() hs.pasteboard.setContents(ipv6) end
             })
         end
-        local macaddr = hs.execute('ifconfig ' .. obj.interface .. ' | grep ether | awk \'{print $2}\'')
+        local macaddr = hs.execute('ifconfig ' .. primary_interface .. ' | grep ether | awk \'{print $2}\'')
         table.insert(menuitems_table, {
-            title = "MAC Addr: " .. macaddr,
-            tooltip = "Copy MAC Address to clipboard",
+            title = "MAC:     " .. macaddr,
+            tooltip = "Copy MAC Addr to clipboard",
             fn = function() hs.pasteboard.setContents(macaddr) end
         })
         -- Start watching the netspeed delta
-        obj.incmd = 'netstat -ibn | grep -e ' .. obj.interface .. ' -m 1 | awk \'{print $7}\''
-        obj.outcmd = 'netstat -ibn | grep -e ' .. obj.interface .. ' -m 1 | awk \'{print $10}\''
+        obj.incmd = 'netstat -ibn | grep -e ' .. primary_interface .. ' -m 1 | awk \'{print $7}\''
+        obj.outcmd = 'netstat -ibn | grep -e ' .. primary_interface .. ' -m 1 | awk \'{print $10}\''
 
-        obj.inseq = hs.execute(obj.incmd)
-        obj.outseq = hs.execute(obj.outcmd)
+        obj.old_ibytes = hs.execute(obj.incmd)
+        obj.old_obytes = hs.execute(obj.outcmd)
 
         if obj.timer then
             obj.timer:stop()
             obj.timer = nil
         end
-        obj.timer = hs.timer.doEvery(1, data_diff)
+        obj.timer = hs.timer.doEvery(1, update)
     end
     table.insert(menuitems_table, {
-        title = "Rescan Network Interfaces",
-        fn = function() obj:rescan() end
+        title = "⟲ Reload",
+        fn = function() obj:reload() end
     })
-    obj.menubar:setTitle("⚠︎")
-    obj.menubar:setMenu(menuitems_table)
+    return menuitems_table
+end
+
+--- SpeedMenu:reload()
+--- Method
+--- Redetect the active interface, darkmode …And redraw everything.
+---
+
+function obj:reload()
+    local menu = menu()
+    obj.menubar:setTitle("⛔")
+    obj.menubar:setMenu(menu)
 end
 
 return obj
